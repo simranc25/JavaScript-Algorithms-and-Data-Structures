9b876e7d2a3fa67fe0fca2895a659266318ccad0
simranc25 <149227284+simranc25@users.noreply.github.com>
2023-12-05 20:42:50 +0530

    Create use-the-delete-keyword-to-remove-object-properties.js

---
+++ Basic Data Structures/use-the-delete-keyword-to-remove-object-properties.js
@@ -0,0 +1,15 @@
+let foods = {
+  apples: 25,
+  oranges: 32,
+  grapes:15,
+  watermelon:20,
+  papaya:13
+};
+
+// Only change code below this line
+delete foods.oranges;
+delete foods.grapes;
+delete foods.papaya;
+// Only change code above this line
+
+console.log(foods);
