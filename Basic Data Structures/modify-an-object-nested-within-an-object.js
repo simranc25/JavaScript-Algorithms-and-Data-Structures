231eb47f213151deedf15943c0feef8caca4f73f
simranc25 <149227284+simranc25@users.noreply.github.com>
2023-12-05 20:34:41 +0530

    Create modify-an-object-nested-within-an-object.js

---
+++ Basic Data Structures/modify-an-object-nested-within-an-object.js
@@ -0,0 +1,14 @@
+let userActivity = {
+  id: 2346935383,
+  date: 'January 17, 2018',
+  data: {
+    totalUsers: 60,
+    online: 50
+  }
+};
+
+// Only change code below this line
+userActivity.data.online = 45;
+// Only change code above this line
+
+console.log(userActivity);
